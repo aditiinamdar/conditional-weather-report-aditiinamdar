fac9ae88521f4941ea93e252982974081403ff57
Aditi Inamdar <66082809+aditiinamdar@users.noreply.github.com>
2026-04-14 10:14:57 +0530

    Refactor App component and add WeatherDisplay

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 
-import React from "react";
-import './../styles/App.css';
+import WeatherDisplay from './WeatherDisplay'
 
-const App = () => {
+function App() {
+  const [weather]=useState({
+    temperature: 25,
+    conditions: 'Sunny',
+  });
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <>
+     <WeatherDisplay weather={weather}/>
+    </>
   )
 }
 
